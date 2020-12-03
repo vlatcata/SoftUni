ce94285905fd1c68cad9350bca8c5cc477bd363d
Vladi <vladi85264@abv.bg>
2020-12-03 16:49:06 +0200

    Added some exercises

---
+++ Programming-Fundamentals/ExamPreparation/EmojiDetector/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberPattern = @"\d";
+            Regex numberRegex = new Regex(numberPattern);
+
+            string emojiPattern = @"(:{2}|\*{2})[A-Z][a-z]{2,}\1";
+            Regex emojiRegex = new Regex(emojiPattern);
+
+            string text = Console.ReadLine();
+
+            long coolThreshhold = 1;
+            numberRegex.Matches(text).Select(x=>x.Value).Select(int.Parse).ToList().ForEach(x => coolThreshhold *= x);
+
+            var matches = emojiRegex.Matches(text);
+            int totalEmojis = matches.Count;
+            List<string> coolEmojis = new List<string>();
+
+            foreach (Match match in matches)
+            {
+               long coolIndex = match.Value.Substring(2, match.Value.Length - 4).ToCharArray().Sum(x=> (int)x);
+
+                if (coolIndex > coolThreshhold)
+                {
+                    coolEmojis.Add(match.Value);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolThreshhold}");
+            Console.WriteLine($"{totalEmojis} emojis found in the text. The cool ones are:");
+
+            foreach (var item in coolEmojis)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals/ExamPreparation/Pirates/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> towns = new Dictionary<string, Dictionary<string, int>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Sail")
+            {
+                string[] tokens = command.Split("||", StringSplitOptions.RemoveEmptyEntries);
+                string town = tokens[0];
+                int population = int.Parse(tokens[1]);
+                int gold = int.Parse(tokens[2]);
+
+                if (towns.ContainsKey(town))
+                {
+                    towns[town]["population"] += population;
+                    towns[town]["gold"] += gold;
+                }
+                else
+                {
+                    towns.Add(town, new Dictionary<string, int>());
+                    towns[town].Add("population", population);
+                    towns[town].Add("gold", gold);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] tokens = command.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+                string town = tokens[1];
+                int people;
+                int gold;
+
+                switch (tokens[0])
+                {
+                    case "Plunder":
+                        people = int.Parse(tokens[2]);
+                        gold = int.Parse(tokens[3]);
+
+                        Console.WriteLine($"{town} plundered! {gold} gold stolen, {people} citizens killed.");
+                        towns[town]["population"] -= people;
+                        towns[town]["gold"] -= gold;
+
+                        if (towns[town]["population"] <= 0 || towns[town]["gold"] <= 0)
+                        {
+                            Console.WriteLine($"{town} has been wiped off the map!");
+                            towns.Remove(town);
+                        }
+                        break;
+                    case "Prosper":
+                        gold = int.Parse(tokens[2]);
+
+                        if (gold < 0)
+                        {
+                            Console.WriteLine("Gold added cannot be a negative number!");
+                        }
+                        else
+                        {
+                            towns[town]["gold"] += gold;
+                            Console.WriteLine($"{gold} gold added to the city treasury. {town} now has {towns[town]["gold"]} gold.");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (towns.Count > 0)
+            {
+                var leftTowns = towns.OrderByDescending(x=>x.Value["gold"]).ThenBy(x=>x.Key);
+                Console.WriteLine($"Ahoy, Captain! There are {towns.Count} wealthy settlements to go to:");
+
+                foreach (var town in leftTowns)
+                {
+                    int population = town.Value["population"];
+                    int gold = town.Value["gold"];
+                    Console.WriteLine($"{town.Key} -> Population: {population} citizens, Gold: {gold} kg");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+        }
+    }
+}
