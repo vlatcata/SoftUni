53e57ac8578abeb3aca632cb5738b8edb101f7d6
Vladi <vladi85264@abv.bg>
2020-12-03 16:56:09 +0200

    Minor improvements

---
+++ Programming-Fundamentals/MidExamPreperation/ExamPrep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Programming-Fundamentals/MidExamPreperation_Exercise/ExamPrepExe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExamPrepExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int totalPeoplePerHour = first + second + third;
+            int hours = 0;
+            while (peopleCount > 0)
+            {
+                peopleCount -= totalPeoplePerHour;
+                hours++;
+
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        } 
+    }
+}
