441935ea521e8e611d2fbacee6359b476913cbb6
Vladi <vladi85264@abv.bg>
2020-12-08 15:51:53 +0200

    Added a program

--- Programming-Fundamentals/ExamPreparation_3/SecretChat/Program.cs
+++ Programming-Fundamentals/ExamPreparation_3/SecretChat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SecretChat
 {
@@ -6,7 +7,61 @@ namespace SecretChat
     {
         static void Main(string[] args)
         {
-            
+            string message = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Reveal")
+            {
+                string[] cmdArg = command.Split(":|:");
+                string action = cmdArg[0];
+
+                if (action == "InsertSpace")
+                {
+                    int index = int.Parse(cmdArg[1]);
+
+                    message = message.Insert(index, " ");
+
+                    Console.WriteLine(message);
+                }
+                else if (action == "Reverse")
+                {
+                    string substrting = cmdArg[1];
+                    
+                    if (message.Contains(substrting))
+                    {
+                        int index = message.IndexOf(substrting);
+                        message = message.Remove(index, substrting.Length);
+
+                        string reversed = new string(substrting.Reverse().ToArray());
+
+                        int startIndex = message.Length;
+                        message = message.Insert(startIndex, reversed);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+
+                    Console.WriteLine(message);
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substrting = cmdArg[1];
+                    string replacement = cmdArg[2];
+
+                    message = message.Replace(substrting, replacement);
+
+                    Console.WriteLine(message);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
         }
     }
+
 }
