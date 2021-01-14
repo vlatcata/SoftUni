322b134123cae4d7e99ec04180fb47fdeb5a2b5c
Vladi <vladi85264@abv.bg>
2021-01-14 17:08:51 +0200

    exe

---
+++ C# Advanced/StackAndQueues_Exercise/StackAndQueues_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueues_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+} 
