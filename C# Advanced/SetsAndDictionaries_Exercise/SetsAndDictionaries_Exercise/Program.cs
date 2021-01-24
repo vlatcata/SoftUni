2548f5c8bb3f89d5f6ade0a3cbb664d4bfb2d463
Vladi <vladi85264@abv.bg>
2021-01-24 16:56:40 +0200

    SetsAndDictionaries_Exercise

---
+++ C# Advanced/SetsAndDictionaries_Exercise/SetsAndDictionaries_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SetsAndDictionaries_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
