be856e522a7267604069a16165c13fa5d3c157fc
Vladi <vladi85264@abv.bg>
2021-01-24 17:09:55 +0200

    StreamsAndDictionaries

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsFilesAndDirectories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamsFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
