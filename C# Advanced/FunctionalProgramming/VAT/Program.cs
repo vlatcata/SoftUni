5bd8e7bee81e16b6becfc08a5e997ca62652b45d
Vladi <vladi85264@abv.bg>
2021-01-27 13:51:19 +0200

    FunctionalProgramming

---
+++ C# Advanced/FunctionalProgramming/VAT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ")
+                .Select(Decimal.Parse)
+                .Select(number => number + (number * 0.2m))
+                .ToArray();
+
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+
+        static decimal[] Select(decimal[] array, Func<decimal,
+            decimal> transformer)
+        {
+            decimal[] newArray = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = transformer(array[i]);
+            }
+
+            return newArray;
+        }
+    }
+}
