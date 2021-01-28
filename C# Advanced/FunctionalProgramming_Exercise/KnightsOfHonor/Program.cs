c9e5fd232a9b586be8ef0405b57c9bc1f552d02e
Vladi <vladi85264@abv.bg>
2021-01-28 20:58:06 +0200

    FunctionalProgramming_Exercise

---
+++ C# Advanced/FunctionalProgramming_Exercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in names)
+            {
+                Console.WriteLine($"Sir {item}");
+            }
+        }
+    }
+}
