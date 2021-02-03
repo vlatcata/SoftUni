e099eda9ba399c8db580c543b54792cb0729083f
Vladi <vladi85264@abv.bg>
2021-02-03 19:03:52 +0200

    DefiningClasses

---
+++ C# Advanced/DefiningClasses/asd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace asd
+{
+    class Program
+    {
+        static void Main()
+        {
+            neznam neznam = new neznam();
+
+            neznam.Name = Console.ReadLine();
+            neznam.Age = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine($"Name is: {neznam.Name} and his age is {neznam.Age}");
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/asd/neznam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asd
+{
+    class neznam
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
