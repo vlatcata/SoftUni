c81ba44dcdf3ff57ed0aae1370ac1c517ce54b57
Vladi <vladi85264@abv.bg>
2021-02-11 18:32:25 +0200

    IterratorsAndComperators

---
+++ C# Advanced/ItteratoraAndComperators/ItteratoraAndComperators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItteratoraAndComperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
