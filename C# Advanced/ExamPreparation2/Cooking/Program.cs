9662c2cef525ad2880c13480dd01079ce84792d0
Vladi <vladi85264@abv.bg>
2021-02-14 18:22:12 +0200

    ExamPreparation2

---
+++ C# Advanced/ExamPreparation2/Cooking/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Stack<int> ingredients = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int ingredientToAddTo = 0;
+            int bread = 0;
+            int cake = 0;
+            int pastry = 0;
+            int fruitPie = 0;
+
+            while (liquids.Count > 0 && ingredients.Count > 0)
+            {
+                int liquid = liquids.Peek();
+                int ingredient = ingredients.Peek();
+                
+
+                int sum = liquid + ingredient;
+
+                if (sum == 25)
+                {
+                    bread++;
+                    liquids.Dequeue();
+                    ingredients.Pop();
+                }
+                else if (sum == 50)
+                {
+                    cake++;
+                    liquids.Dequeue();
+                    ingredients.Pop();
+                }
+                else if (sum == 75)
+                {
+                    pastry++;
+                    liquids.Dequeue();
+                    ingredients.Pop();
+                }
+                else if (sum == 100)
+                {
+                    fruitPie++;
+                    liquids.Dequeue();
+                    ingredients.Pop();
+                }
+                else
+                {
+                    liquids.Dequeue();
+                    ingredientToAddTo = ingredients.Pop() + 3;
+                    ingredients.Push(ingredientToAddTo);
+                }
+            }
+
+            if (bread >= 1 && cake >= 1 && pastry >= 1 && fruitPie >= 1)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Count <= 0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+
+            if (ingredients.Count <= 0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingredients)}");
+            }
+
+            Console.WriteLine($"Bread: {bread}");
+            Console.WriteLine($"Cake: {cake}");
+            Console.WriteLine($"Fruit Pie: {fruitPie}");
+            Console.WriteLine($"Pastry: {pastry}");
+        }
+    }
+}
---
+++ C# Advanced/ExamPreparation2/БакеръОпенинг/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace БакеръОпенинг
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
