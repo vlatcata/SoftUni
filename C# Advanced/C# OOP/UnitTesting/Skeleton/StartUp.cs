a6b59c20b0bc0979d71b76cf2e102b71eec55c47
Vladi <vladi85264@abv.bg>
2021-03-21 14:34:36 +0200

    UnitTesting

---
+++ C# Advanced/C# OOP/UnitTesting/Skeleton/StartUp.cs
@@ -0,0 +1,9 @@
+﻿public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Hero hero = new Hero("ivan");
+        Axe axe = new Axe(5, 5);
+        Dummy dummy = new Dummy(20, 5);
+    }
+}
