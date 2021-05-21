62abc5fb0e7151bc5f392d6f6f2fb369cf385a37
Vladi <vladi85264@abv.bg>
2021-05-21 13:21:14 +0300

    JS Advanced

---
+++ JS Advanced/Syntax, Functions and Statements/MathOperations.js
@@ -0,0 +1,25 @@
+function solve(num1, num2, input){
+    let result;
+    if (input == '+') {
+        result = num1 + num2;
+    }
+    else if (input == '-') {
+        result = num1 - num2;
+    }
+    else if (input == '*') {
+        result = num1 * num2;
+    }
+    else if (input == '/') {
+        result = num1 / num2;
+    }
+    else if (input == '%') {
+        result = num1 % num2;
+    }
+    else if (input == '**') {
+        result = num1 ** num2;
+    }
+
+    console.log(result);
+}
+
+solve(2, 3, '+');
