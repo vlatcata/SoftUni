b7c3aa7eacbc6e611dd61d732f5e8458928f5a11
Vladi <vladi85264@abv.bg>
2021-05-21 12:54:36 +0300

    JavaScript Advanced

---
+++ JS Advanced/Syntax, Functions ann Statements/DayOfWeek.js
@@ -0,0 +1,28 @@
+function solve(dayOfWeek){
+    if (dayOfWeek == 'Monday') {
+        console.log(1);
+    }
+    else if (dayOfWeek == 'Tuesday') {
+        console.log(2);
+    }
+    else if (dayOfWeek == 'Wednesday') {
+        console.log(3);
+    }
+    else if (dayOfWeek == 'Thursday') {
+        console.log(4);
+    }
+    else if (dayOfWeek == 'Friday') {
+        console.log(5);
+    }
+    else if (dayOfWeek == 'Saturday') {
+        console.log(6);
+    }
+    else if (dayOfWeek == 'Sunday') {
+        console.log(7);
+    }
+    else {
+        console.log('error');
+    }
+}
+
+solve('Monday');
---
+++ JS Advanced/Syntax, Functions ann Statements/LargestNumber.js
@@ -0,0 +1,6 @@
+function solve(arg1, arg2, arg3){
+    let maxNum = Math.max(arg1, arg2, arg3);
+    console.log(`The largest number is ${maxNum}.`);
+}
+
+solve(1, 2, 3);
