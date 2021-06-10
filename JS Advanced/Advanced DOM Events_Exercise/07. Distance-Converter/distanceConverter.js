71f78f8fa7174f05056dc98d5fafcfc08a1cc5ff
Vladi <vladi85264@abv.bg>
2021-06-10 11:52:33 +0300

    Advanced Functions

--- JS Advanced/Advanced DOM Events_Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Advanced/Advanced DOM Events_Exercise/07. Distance-Converter/distanceConverter.js
@@ -4,29 +4,42 @@ function attachEventsListeners() {
     button.addEventListener('click', (e) => {
         let fromUnit = document.getElementById('inputUnits').value;
         let toUnit = document.getElementById('outputUnits').value;
-    
+
         let input = Number(document.getElementById('inputDistance').value);
         let output = document.getElementById('outputDistance');
-        
+
         let unitToM = 0;
         let result = 0;
 
         if (fromUnit === 'km') {
             unitToM = input * 1000;
-        } else if(fromUnit === 'm') {
-            unitToM = fromUnit;
+
+        } else if (fromUnit === 'm') {
+            unitToM = input;
+
         } else if (fromUnit === 'cm') {
-            unitToM = fromUnit * 0.01;
+
+            unitToM = input * 0.01;
+
         } else if (fromUnit === 'mm') {
-            unitToM = fromUnit * 0.001;
+
+            unitToM = input * 0.001;
+
         } else if (fromUnit === 'mi') {
-            unitToM = fromUnit * 1609.34;
-        } else if (unitToM === 'yrd') {
-            unitToM = fromUnit * 0.9144;
+
+            unitToM = input * 1609.34;
+
+        } else if (fromUnit === 'yrd') {
+
+            unitToM = input * 0.9144;
+
         } else if (fromUnit === 'ft') {
-            unitToM = fromUnit * 0.3048;
+
+            unitToM = input * 0.3048;
+
         } else if (fromUnit === 'in') {
-            unitToM = fromUnit * 0.0254;
+
+            unitToM = input * 0.0254;
         }
 
 
