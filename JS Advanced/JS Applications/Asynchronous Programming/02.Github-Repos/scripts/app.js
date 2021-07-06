54eafcce63d7778692c3cc036c061a48fa0d7971
Vladi <vladi85264@abv.bg>
2021-07-06 12:14:43 +0300

    tasks

--- JS Advanced/JS Applications/Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,20 @@
 function loadRepos() {
-	console.log("TODO...");
-}
+	let button = document.querySelector("body button");
+	let url = "https://api.github.com/users/k1r1L/repos";
+	let ulElement = document.getElementById("repos");
+	ulElement.firstChild.remove()
+	button.addEventListener("click", () => {
+	  fetch(url)
+		.then((response) => response.json())
+		.then((data) =>
+		  data.forEach((obj) => {
+			let liElement = document.createElement("li");
+			let anchorElement = document.createElement("A");
+			anchorElement.setAttribute("href", obj.html_url);
+			anchorElement.textContent = obj.full_name
+			liElement.appendChild(anchorElement);
+			ulElement.appendChild(liElement);
+		  })
+		);
+	});
+  }
--- JS Advanced/JS Applications/Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,24 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username');
+    let repository = document.getElementById(`repo`);
+    let ulCommits = document.getElementById('commits');
+
+    let url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+    ulCommits.textContent = '';
+
+    fetch(url)
+    .then((data) => data.json())
+    .then(commits => {
+        commits.forEach(e => {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${e.commit.author.name}: ${e.commit.message}`;
+            ulCommits.appendChild(liElement);
+        });
+    })
+    .catch(error => {
+        let liItem = document.createElement('li');
+        liItem.textContent = `Error: ${error.status} (Not Found)`;
+        ulCommits.appendChild(liItem);
+    })
 }
