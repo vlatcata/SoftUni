c486c0a3c3c44342894db27457808ebc1d35a5e5
Vladi <vladi85264@abv.bg>
2021-09-28 15:50:46 +0300

    Subsequences and Joins Tasks

---
+++ C# DB/MS SQL/Subqueries and Joins/GeographyDB Tasks.sql
@@ -0,0 +1,37 @@
+SELECT c.CountryCode, m.MountainRange, p.PeakName, p.Elevation
+FROM Peaks AS p
+JOIN Mountains AS m ON p.MountainId = m.Id
+JOIN MountainsCountries AS mc ON mc.MountainId = m.Id
+JOIN Countries AS c ON mc.CountryCode = c.CountryCode
+WHERE c.CountryCode = 'BG' AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
+
+SELECT mc.CountryCode, COUNT(m.MountainRange) AS MountainRanges
+FROM MountainsCountries AS mc
+JOIN Mountains AS m ON mc.MountainId = m.Id
+WHERE mc.CountryCode IN ('BG', 'US', 'RU')
+GROUP BY mc.CountryCode
+
+SELECT TOP(5) c.CountryName, r.RiverName
+FROM CountriesRivers AS cr
+JOIN Rivers AS r ON cr.RiverId = r.Id
+RIGHT JOIN Countries AS c ON cr.CountryCode = c.CountryCode
+WHERE c.ContinentCode = 'AF'
+ORDER BY c.CountryName ASC
+
+SELECT COUNT(*) AS [Count]
+FROM Countries AS c
+LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+LEFT JOIN Mountains AS m ON m.Id = mc.MountainId
+WHERE m.Id IS NULL
+
+SELECT TOP(5) c.CountryName,
+MAX(p.Elevation) AS HighestPeakElevation,
+MAX(r.[Length]) AS LongestRiverLength
+FROM Countries AS c
+JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
+JOIN Peaks AS p ON p.MountainId = mc.MountainId
+JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
+JOIN Rivers AS r ON r.Id = cr.RiverId
+GROUP BY c.CountryName
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName
---
+++ C# DB/MS SQL/Subqueries and Joins/SoftuniDB Tasks.sql
@@ -0,0 +1,68 @@
+SELECT TOP(5) e.EmployeeID, e.JobTitle, e.AddressID, a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+ORDER BY a.AddressID ASC
+
+SELECT TOP(50) e.FirstName, e.LastName, t.[Name], a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY e.FirstName, e.LastName
+
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.[Name]
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+AND d.[Name] = 'Sales'
+ORDER BY e.EmployeeID ASC
+
+SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.Name
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY d.DepartmentID ASC
+
+SELECT TOP(3) e.EmployeeID, e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS p ON e.EmployeeID = p.EmployeeID
+WHERE p.ProjectID IS NULL
+ORDER BY e.EmployeeID ASC
+
+SELECT e.FirstName, e.LastName, e.HireDate, d.[Name]
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.HireDate > '1999-01-01' AND d.[Name] IN ('Sales', 'Finance')
+ORDER BY e.HireDate ASC
+
+SELECT TOP(5) e.EmployeeID, e.FirstName, p.[Name]
+FROM Employees AS e
+JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+ORDER BY e.EmployeeID ASC
+
+SELECT e.EmployeeID, e.FirstName,
+CASE 
+	WHEN YEAR(p.StartDate) >= 2005 THEN NULL
+	ELSE p.[Name]
+END AS ProjectName
+FROM Employees AS e
+JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE e.EmployeeID = 24
+
+SELECT E.EmployeeID, E.FirstName, E.ManagerID, M.FirstName AS ManagerName
+FROM Employees AS E
+JOIN Employees AS M ON M.EmployeeID = E.ManagerID 
+WHERE E.ManagerID IN (3, 7)
+ORDER BY E.EmployeeID
+
+SELECT TOP(50) e.EmployeeID, e.FirstName + ' ' + e.LastName AS EmployeeName, es.FirstName + ' ' + es.LastName AS ManagerName, d.[Name] AS DepartmentName
+FROM Employees AS e
+JOIN Employees AS es ON es.EmployeeID = e.ManagerID
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+ORDER BY e.EmployeeID ASC
+
+SELECT TOP(1) AVG(Salary) AS AverageSalary
+FROM Employees AS e
+GROUP BY e.DepartmentID
+ORDER BY AverageSalary
