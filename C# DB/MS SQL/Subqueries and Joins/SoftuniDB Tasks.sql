63032adfc8092995f881caf1e833bffadb6f0682
Vladi <vladi85264@abv.bg>
2021-09-28 20:55:15 +0300

    Changed the syntax in 1 task

--- C# DB/MS SQL/Subqueries and Joins/SoftuniDB Tasks.sql
+++ C# DB/MS SQL/Subqueries and Joins/SoftuniDB Tasks.sql
@@ -30,7 +30,7 @@ ORDER BY e.EmployeeID ASC
 SELECT e.FirstName, e.LastName, e.HireDate, d.[Name]
 FROM Employees AS e
 JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-WHERE e.HireDate > '1999-01-01' AND d.[Name] IN ('Sales', 'Finance')
+WHERE e.HireDate > '01-01-1999' AND d.[Name] IN ('Sales', 'Finance')
 ORDER BY e.HireDate ASC
 
 SELECT TOP(5) e.EmployeeID, e.FirstName, p.[Name]
