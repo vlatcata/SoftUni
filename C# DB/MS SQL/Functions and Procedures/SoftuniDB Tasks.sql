f9829f45d791eca19a972aff695fc8ed058bd8ee
Vladi <vladi85264@abv.bg>
2021-10-06 22:21:37 +0300

    Added two more tasks

--- C# DB/MS SQL/Functions and Procedures/SoftuniDB Tasks.sql
+++ C# DB/MS SQL/Functions and Procedures/SoftuniDB Tasks.sql
@@ -98,4 +98,49 @@ GO
 
 -- 08 TASK
 
-
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment (@departmentId INT)
+AS
+BEGIN
+    ---First we need to delete all records from EmployeesProjects where EmployeeID is one of the lately deleted
+    DELETE FROM [EmployeesProjects]
+    WHERE [EmployeeID] IN (
+                            SELECT [EmployeeID]
+                              FROM [Employees]
+                             WHERE [DepartmentID] = @departmentId
+                          )
+    
+    ---We need to set ManagerID to NULL of all Employees which have their Manager lately deleted
+    UPDATE [Employees]
+    SET [ManagerID] = NULL
+    WHERE [ManagerID] IN (
+                            SELECT [EmployeeID]
+                              FROM [Employees]
+                             WHERE [DepartmentID] = @departmentId
+                          )
+ 
+    ---We need to alter ManagerID column from Departments in order to be nullable because we need to set
+    ---ManagerID to NULL of all Departments that have their Manager lately deleted
+    ALTER TABLE [Departments]
+    ALTER COLUMN [ManagerID] INT
+ 
+    ---We need to set ManagerID to NULL (no Manager) to all departments that have their Manager lately deleted
+    UPDATE [Departments]
+    SET [ManagerID] = NULL
+    WHERE [ManagerID] IN (
+                            SELECT [EmployeeID]
+                              FROM [Employees]
+                             WHERE [DepartmentID] = @departmentId
+                          )
+    
+    ---We need to delete all employees from the lately deleted department
+    DELETE FROM [Employees]
+    WHERE [DepartmentID] = @departmentId
+ 
+    ---Lastly we delete wanted department
+    DELETE FROM [Departments]
+    WHERE [DepartmentID] = @departmentId
+ 
+    SELECT COUNT(*)
+      FROM [Employees]
+     WHERE [DepartmentID] = @departmentId
+END
