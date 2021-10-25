60800a2a97f8d7a51103786aaa5814985cae91c2
Vladi <vladi85264@abv.bg>
2021-10-26 00:01:05 +0300

    Entity Framework Introduction

---
+++ C# DB/ENTITY FRAMEWORK CORE/Entity Framework Introduction/Entrity Framework Core/Projects.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Entrity_Framework_Core
+{
+    public partial class Projects
+    {
+        public Projects()
+        {
+            EmployeesProjects = new HashSet<EmployeesProjects>();
+        }
+
+        [Key]
+        [Column("ProjectID")]
+        public int ProjectId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        [Column(TypeName = "smalldatetime")]
+        public DateTime StartDate { get; set; }
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? EndDate { get; set; }
+
+        [InverseProperty("Project")]
+        public virtual ICollection<EmployeesProjects> EmployeesProjects { get; set; }
+    }
+}
