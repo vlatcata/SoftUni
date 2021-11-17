922c3442aecc37f2f75d66ff79e2456f25171e24
Vladi <vladi85264@abv.bg>
2021-11-17 14:50:58 +0200

    Finished the XML Imports

---
+++ C# DB/ENTITY FRAMEWORK CORE/XML Processing/ProductShopDb/ProductShop/DTOs/Import/CategoryProductImportDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    [XmlType("CategoryProduct")]
+    public class CategoryProductImportDto
+    {
+        [XmlElement("CategoryId")]
+        public int CategoryId { get; set; }
+
+        [XmlElement("ProductId")]
+        public int ProductId { get; set; }
+    }
+}
---
+++ C# DB/ENTITY FRAMEWORK CORE/XML Processing/ProductShopDb/ProductShop/DTOs/Import/ProductImportDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    [XmlType("Product")]
+    public class ProductImportDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("sellerId")]
+        public int SellerId { get; set; }
+
+        [XmlElement("buyerId")]
+        public int? BuyerId { get; set; }
+    }
+}
