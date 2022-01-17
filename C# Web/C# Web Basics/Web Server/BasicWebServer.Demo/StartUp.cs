9669523c2fb1c022e947760859e4f56bc1639d63
Vladi <vladi85264@abv.bg>
2022-01-17 14:23:33 +0200

    Downloadable file functionality

--- C# Web/C# Web Basics/Web Server/BasicWebServer.Demo/StartUp.cs
+++ C# Web/C# Web Basics/Web Server/BasicWebServer.Demo/StartUp.cs
@@ -11,13 +11,26 @@ public class StartUp
 <input type='submit' value ='Save' />
 </form>";
 
+    private const string DownloadForm = @"<form action='/Content' method='POST'>
+   <input type='submit' value ='Download Sites Content' /> 
+</form>";
+
+    private const string FileName = "content.txt";
+
     public static async Task Main()
-    => await new HttpServer(routes => routes
+    {
+        await DownloadSitesAsTextFile(FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+
+        var server = new HttpServer(routes => routes
         .MapGet("/", new TextResponse("Hello from the server!"))
         .MapGet("/HTML", new HtmlResponse(HtmlForm))
         .MapPost("/HTML", new TextResponse("", AddFormDataAction))
-        .MapGet("/Redirect", new RedirectResponse("https://softuni.org/")))
-        .Start();
+        .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
+        .MapGet("/Content", new HtmlResponse(DownloadForm))
+        .MapPost("/Content", new TextFileResponse(FileName)));
+
+        await server.Start();
+    }
 
     private static void AddFormDataAction(Request request, Response response)
     {
@@ -28,5 +41,34 @@ public class StartUp
             response.Body += $"{key} - {value}";
             response.Body += Environment.NewLine;
         }
+    }
+
+    private static async Task<string> DownloadWebSiteContent(string url)
+    {
+        var httpClient = new HttpClient();
+        using (httpClient)
+        {
+            var response = await httpClient.GetAsync(url);
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            return html.Substring(0, 2000);
+        }
+    }
+
+    private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+    {
+        var downloads = new List<Task<string>>();
+
+        foreach (var url in urls)
+        {
+            downloads.Add(DownloadWebSiteContent(url));
+        }
+
+        var responses = await Task.WhenAll(downloads);
+
+        var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
+
+        await File.WriteAllTextAsync(fileName, responsesString);
     }
 }
--- C# Web/C# Web Basics/Web Server/BasicWebServer.Server/HTTP/Header.cs
+++ C# Web/C# Web Basics/Web Server/BasicWebServer.Server/HTTP/Header.cs
@@ -14,6 +14,7 @@ namespace BasicWebServer.Server.HTTP
         public const string Date = "Date";
         public const string Location = "Location";
         public const string Server = "Server";
+        public const string ContentDisposition = "Content-Disposition";
 
         public Header(string name, string value)
         {
