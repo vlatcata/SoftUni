565d5d0f1c51e2ea50ce213728a3ae6dcb6ca330
Vladi <vladi85264@abv.bg>
2022-03-06 17:42:53 +0200

    Added a custom attribute

---
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/CustomAttributes/IsBefore.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCBuilder.Core.CustomAttributes
+{
+    public class IsBefore : ValidationAttribute
+    {
+        private readonly string propertyToCompare;
+
+        public IsBefore(string _propertyToCompare, string errorMessage = "")
+        {
+            propertyToCompare = _propertyToCompare;
+            ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime datetoCompare = (DateTime)validationContext
+                .ObjectType
+                .GetProperty(propertyToCompare)
+                .GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < datetoCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception)
+            {}
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
