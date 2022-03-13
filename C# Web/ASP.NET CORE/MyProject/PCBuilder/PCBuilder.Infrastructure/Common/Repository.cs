4c5a5cbad99e51bf89a3a14abe72c13c108e20b5
Vladi <vladi85264@abv.bg>
2022-03-13 18:00:33 +0200

    Cleaned up usings

--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Infrastructure/Common/Repository.cs
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Infrastructure/Common/Repository.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace PCBuilder.Infrastructure.Data.Common
 {
