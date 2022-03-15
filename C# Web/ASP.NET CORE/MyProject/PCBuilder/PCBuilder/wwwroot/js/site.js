552ef028d031984181066e7d545613d62b8c7c28
Vladi <vladi85264@abv.bg>
2022-03-15 15:47:18 +0200

    Added views for the cart

--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder/wwwroot/js/site.js
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//let createSpeciticationButton = document.getElementsByClassName("create-specification");
+
+//createSpeciticationButton.onClick(c => document.getElementsByClassName("form-group").innerHTML += "<label for="inputSpecifications">Name</label>
+//    < input type = "text" id = "inputSpecifications" class= "form-control" >
+//                                <label for="inputSpecifications">Description</label>
+//                                <input type="text" id="inputSpecifications" class="form-control">")
+
