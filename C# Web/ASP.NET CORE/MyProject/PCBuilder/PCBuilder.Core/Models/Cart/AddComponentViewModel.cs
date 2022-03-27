a6d79db3d7a9db267a50c2cfd08922543acd07f1
Vladi <vladi85264@abv.bg>
2022-03-27 14:22:48 +0300

    Some changes

--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/AddComponentViewModel.cs
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/AddComponentViewModel.cs
@@ -9,13 +9,13 @@
 
         public Guid Id { get; set; }
 
-        public string? Category { get; set; }
+        public string Category { get; set; }
 
-        public string? Model { get; set; }
+        public string Model { get; set; }
 
-        public string? Manufacturer { get; set; }
+        public string Manufacturer { get; set; }
 
-        public string? ImageUrl { get; set; }
+        public string ImageUrl { get; set; }
 
         public decimal Price { get; set; }
 
--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/SpecificationsViewModel.cs
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/SpecificationsViewModel.cs
@@ -4,8 +4,8 @@
     {
         public Guid Id { get; set; }
 
-        public string? Title { get; set; }
+        public string Title { get; set; }
 
-        public string? Description { get; set; }
+        public string Description { get; set; }
     }
 }
