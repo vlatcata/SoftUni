58c9d5070136bb351f222d58833aafd8440248f9
Vladi <vladi85264@abv.bg>
2022-03-31 15:59:27 +0300

    Edited the custom messages

--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/AddComponentViewModel.cs
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/AddComponentViewModel.cs
@@ -11,19 +11,23 @@ namespace PCBuilder.Core.Models.Cart
 
         public Guid Id { get; set; }
 
-        [StringLength(100, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
+        [Required]
+        [StringLength(40, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string Category { get; set; }
 
         [Required]
-        [StringLength(60, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
+        [StringLength(40, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string Model { get; set; }
 
-        [StringLength(100, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
+        [Required]
+        [StringLength(40, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string Manufacturer { get; set; }
 
+        [Required]
         [StringLength(250, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string ImageUrl { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
         public List<SpecificationsViewModel> Specifications { get; set; }
--- C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/SpecificationsViewModel.cs
+++ C# Web/ASP.NET CORE/MyProject/PCBuilder/PCBuilder.Core/Models/Cart/SpecificationsViewModel.cs
@@ -6,9 +6,11 @@ namespace PCBuilder.Core.Models.Cart
     {
         public Guid Id { get; set; }
 
+        [Required]
         [StringLength(100, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string Title { get; set; }
 
+        [Required]
         [StringLength(100, ErrorMessage = "{0} cannot be empty or more than {1} symbols")]
         public string Description { get; set; }
     }
